2829633de7a2e583fe04f5a42c3aef12f0806c00
Liran Ramekar <liranramekar7@gmail.com>
2024-03-22 09:51:08 +0530

    new addtion

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -55,7 +55,7 @@ const loginUser = async (req, res) => {
           path: "/",
           domain: "crypto-app-inf1.onrender.com",
           httpOnly: true,
-          secure : true,
+          secure : false,
         }).status(201).send({ status: true, user: updated, msg: "Logged in successfully" })
 
       } else {
