86a1c577d16633a2650a333a8854a2b1a5d2e601
Liran Ramekar <liranramekar7@gmail.com>
2024-03-26 21:41:55 +0530

    added

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -52,7 +52,7 @@ const loginUser = async (req, res) => {
           { returnOriginal: false })
  
         return res.cookie("token", token.accessToken, {
-          path: "/api",
+          path: "/",
           domain: "crypto-app-inf1.onrender.com",
           httpOnly: true,
           secure : true,
